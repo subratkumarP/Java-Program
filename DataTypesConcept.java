01086b8c5c6f9fd4e81283f2c496678b78a9679f
unknown <azurepal21@gmail.com>
2024-07-27 21:16:38 +0530

    First java File

---
+++ DataTypesConcept.java
@@ -0,0 +1,21 @@
+package JavaConceptLearning;
+
+public class DataTypesConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Hi , This my java , I am very Happy Today
+		//Data Type: Type of data, String, Char, Interger
+		
+		//Primitive: no need to create a object 
+		//Numeric Type
+		//1. Integral Type
+					//1. Integer
+					//2. Floating 
+		//
+		//Non Primitive: Need to create object -class, array, interface, abs
+		
+	}
+
+}
